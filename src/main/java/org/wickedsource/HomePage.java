1345e4bb2a8275315785faa11195af3957da60a4
Tom Hombergs <tom.hombergs@gmail.com>
2013-05-19 00:18:47 +0200

    initial checkin

---
+++ src/main/java/org/wickedsource/HomePage.java
@@ -0,0 +1,16 @@
+package org.wickedsource;
+
+import org.apache.wicket.request.mapper.mount.Mount;
+import org.apache.wicket.request.mapper.mount.MountParameters;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+	}
+}
